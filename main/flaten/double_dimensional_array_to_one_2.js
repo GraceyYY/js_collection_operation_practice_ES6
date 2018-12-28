be40290d213954cfafff9e2601fc43789271a67d
Grace <lunay0325@gmail.com>
2018-12-28 23:06:25 +0800

    Complete the practices in flaten folder

--- main/flaten/double_dimensional_array_to_one_2.js
+++ main/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+  let flatArr = collection.reduce((flat, item) => {
+    return flat.concat(item);
+  });
+  let noRepeat = new Set(flatArr);
+  let result = [];
+  for (let item of noRepeat) {
+    result.push(item);
+  }
+  return result;
 }
-
 module.exports = double_to_one;
