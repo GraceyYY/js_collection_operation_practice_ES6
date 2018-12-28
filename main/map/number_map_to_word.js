446216719483a3ff77082610e12be02beaec1bfb
Grace <lunay0325@gmail.com>
2018-12-28 23:15:37 +0800

    Complete the practices in map folder

--- main/map/number_map_to_word.js
+++ main/map/number_map_to_word.js
@@ -1,6 +1,15 @@
 'use strict';
-var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+const LETTER = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var number_map_to_word = function(collection) {
+  return collection.map(item => getLetter(item));
 };
 
+function getLetter(index) {
+  if (index <= 26) {
+    return LETTER[index - 1];
+  } else {
+    let int = Math.floor((index - 1) / 26);
+    return getLetter(int) + getLetter(index - int * 26);
+  }
+}
 module.exports = number_map_to_word;
